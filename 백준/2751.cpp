0f8f6bd30be0624953702598af8eee5ddd924edb
tasddc1226 <tasddc@naver.com>
2021-09-06 17:45:23 +0900

    [9/6] Boj solve : sort

---
+++ 백준/2751.cpp
@@ -0,0 +1,24 @@
+/*
+ 	date : 2021.09.06 
+    problom : 2751
+	title : 수 정렬하기 2
+ */
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define all(v) v.begin(),v.end()
+using namespace std;
+
+int main()
+{
+    
+  int N, x;
+  cin >> N;
+  vector<int> v;
+  for (int i=0; i<N; i++){
+    cin >> x;
+    v.push_back(x);
+  }
+  sort(all(v));
+  for (int i:v) cout << i << "\n";
+}
