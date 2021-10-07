df4c2346f796669c62a3fb81b4f4d8dc3a60a7e6
tasddc1226 <tasddc@naver.com>
2021-10-07 17:54:08 +0900

    [update] Boj_숫자 카드 2

---
+++ 백준/10816.py
@@ -0,0 +1,30 @@
+# 	date : 2021.10.07
+# 	problom : 10816
+# 	title : 백준_숫자 카드2
+#   discribe : 정수 숫자 카드를 몇 개 가지고 있는지 구해보자
+
+from sys import stdin
+_ = stdin.readline()
+N = sorted(map(int,stdin.readline().split()))
+_ = stdin.readline()
+M = map(int,stdin.readline().split())
+
+def binary(n, N, start, end):
+    if start > end:
+        return 0
+    m = (start+end)//2
+    if n == N[m]:
+        return N[start:end+1].count(n)
+    elif n < N[m]:
+        return binary(n, N, start, m-1)
+    else:
+        return binary(n, N, m+1, end)
+
+n_dic = {}
+for n in N:
+    start = 0
+    end = len(N) - 1
+    if n not in n_dic:
+        n_dic[n] = binary(n, N, start, end)
+
+print(' '.join(str(n_dic[x]) if x in n_dic else '0' for x in M ))
