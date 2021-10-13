feca8f0fa4a6ff18577146034aadca0097de13f5
tasddc1226 <tasddc@naver.com>
2021-10-13 12:56:03 +0900

    [fix] Boj_이항 계수 1 DP

--- 백준/정수론 및 조합론/11050.cpp
+++ 백준/정수론 및 조합론/11050.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <algorithm>
+#define MAX 10
 using namespace std;
 static int binomial(int n, int k) {
     
@@ -15,13 +16,31 @@ static int binomial(int n, int k) {
     return binomial(n-1, k-1) + binomial(n-1,k);
 }
 
+static int binomial_coefficient(int n, int k){
+    int dp[MAX][MAX];
+    int m;
+    for (int i=0; i <= n; i++){
+        m = (i < k)? i : k;
+        for (int j=0; j <= m; j++){
+            if (j == 0 || j == i) dp[i][j] = 1;
+            else dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+        }
+    }
+    return dp[n][k];
+}
+
 int main()
 {
     // 1<= N <=10 , 0<= K <= N
+    ios_base::sync_with_stdio(false); 
+	cin.tie(NULL);
+
     int N, K;
     cin >> N >> K;
     
-    int res = binomial(N, K);
-    cout << res;
+    
+
+    // cout << binomial(N, K) << "\n"; // 재귀
+    cout << binomial_coefficient(N, K); // dp
     
 }
