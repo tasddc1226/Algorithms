694f2ac851db2928cea9145043821f45a00d40b4
tasddc1226 <tasddc@naver.com>
2021-10-14 22:30:10 +0900

    [chore] 코드 정리

--- 백준/1차원 배열/8958.cpp
+++ 백준/1차원 배열/8958.cpp
@@ -7,28 +7,27 @@
 */
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string.h>
-#define all(v) v.begin(), v.end()
+
 using namespace std;
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	char h[80]; // 퀴즈 정답 OX 문자열
-	int res = 0; // 결과
-	int acc = 0; // 정답 수
-    int loop = 0; // loop count
-	int n; // test case
-	cin >> n;
-	while (n > loop) {
+	
+	char h[80]; 	 // 퀴즈 정답 OX 문자열
+	int t, res, acc; // test case, 결과, 정답 수
+	cin >> t;
+	while (t--) {
+	    res = 0; acc = 0;
 		cin >> h;
-		for (int i=0; i<strlen(h); i++) {
-			if (h[i]=='O') {
+		for (size_t i = 0; i < strlen(h); i++) {
+			if ( h[i] == 'O' ) {
 				acc++; res += acc;
-			}else acc = 0;
+			} else {
+			    acc = 0;
+			}
 		}
 		cout<<res<<"\n";
-		res = 0;acc = 0;loop++;
 	}
 }
